b0da6b4c1c99b60bf87b4430c60a01f5b0df075a
Brian Sze Kai Cheong <skcheongbrian@gmail.com>
2026-08-14 17:23:08 +0800

    Implement diagnostics

--- src/Fixen/LSP/Server.hs
+++ src/Fixen/LSP/Server.hs
@@ -6,12 +6,14 @@ module Fixen.LSP.Server (
 ) where
 
 import Control.Monad.IO.Class (liftIO)
+import Data.Text qualified as Text
+import System.IO (hPutStrLn, stderr)
+
 import Language.LSP.Protocol.Message
 import Language.LSP.Protocol.Types
 import Language.LSP.Server
-import System.IO (hPutStrLn, stderr)
-import Data.Text qualified as Text
 
+import Fixen.Diagnostics qualified as Diagnostics
 import Fixen.Monad (runFixenM)
 import Fixen.Pipeline (pipeline)
 
@@ -30,19 +32,20 @@ serverDefinition =
     , configSection = "fixen"
     , doInitialize = \environment _request ->
         pure $ Right environment
-    , staticHandlers = \_clientCapabilities -> handlers
+    , staticHandlers = const handlers
     , interpretHandler = \environment ->
         Iso (runLspT environment) liftIO
-    , options = defaultOptions
-                  { optTextDocumentSync =
-                      Just $
-                        TextDocumentSyncOptions
-                          (Just True)
-                          (Just TextDocumentSyncKind_Full)
-                          (Just False)
-                          (Just False)
-                          Nothing
-                  }
+    , options =
+        defaultOptions
+          { optTextDocumentSync =
+              Just $
+                TextDocumentSyncOptions
+                  (Just True)
+                  (Just TextDocumentSyncKind_Full)
+                  (Just False)
+                  (Just False)
+                  Nothing
+          }
     }
 
 handlers :: Handlers (LspM Config)
@@ -51,7 +54,6 @@ handlers =
     [ notificationHandler SMethod_Initialized $ \_notification ->
         liftIO $
           hPutStrLn stderr "fixen-lsp initialized"
-
     , notificationHandler SMethod_TextDocumentDidOpen $ \notification -> do
         let TNotificationMessage _ _ params = notification
             DidOpenTextDocumentParams document = params
@@ -61,7 +63,6 @@ handlers =
           hPutStrLn stderr ("version: " <> show version)
           hPutStrLn stderr ("characters: " <> show (Text.length contents))
         analyzeDocument uri contents
-
     , notificationHandler SMethod_TextDocumentDidChange $ \notification -> do
         let TNotificationMessage _ _ params = notification
             DidChangeTextDocumentParams document changes = params
@@ -76,27 +77,101 @@ handlers =
 changeText :: TextDocumentContentChangeEvent -> Text.Text
 changeText (TextDocumentContentChangeEvent (InR wholeDocument)) =
   let TextDocumentContentChangeWholeDocument contents = wholeDocument
-    in contents
-
+   in contents
 changeText (TextDocumentContentChangeEvent (InL partialChange)) =
   let TextDocumentContentChangePartial _range _rangeLength contents = partialChange
-    in contents
+   in contents
+
+toLspPosition :: Int -> Int -> Position
+toLspPosition line column =
+  Position
+    (fromIntegral (max 0 (line - 1)))
+    (fromIntegral (max 0 (column - 1)))
+
+toLspRange :: Maybe Diagnostics.FixenSourceSpan -> Range
+toLspRange maybeSpan =
+  case maybeSpan of
+    Nothing ->
+      Range
+        (Position 0 0)
+        (Position 0 0)
+    Just span ->
+      Range
+        ( toLspPosition
+            (Diagnostics.fixenStartLine span)
+            (Diagnostics.fixenStartColumn span)
+        )
+        ( toLspPosition
+            (Diagnostics.fixenEndLine span)
+            (Diagnostics.fixenEndColumn span)
+        )
+
+toLspSeverity :: Diagnostics.FixenDiagnosticSeverity -> DiagnosticSeverity
+toLspSeverity Diagnostics.FixenDiagnosticError = DiagnosticSeverity_Error
+toLspSeverity Diagnostics.FixenDiagnosticWarning = DiagnosticSeverity_Warning
+
+toLspDiagnostic :: Diagnostics.FixenDiagnostic -> Diagnostic
+toLspDiagnostic diagnostic =
+  Diagnostic
+    { _range =
+        toLspRange (Diagnostics.fixenDiagnosticSpan diagnostic)
+    , _severity =
+        Just
+          ( toLspSeverity
+              (Diagnostics.fixenDiagnosticSeverity diagnostic)
+          )
+    , _code = Nothing
+    , _codeDescription = Nothing
+    , _source = Just "fixen"
+    , _message = Diagnostics.fixenDiagnosticMessage diagnostic
+    , _tags = Nothing
+    , _relatedInformation = Nothing
+    , _data_ = Nothing
+    }
+
+sendDocumentDiagnostics :: Uri -> [Diagnostic] -> LspM Config ()
+sendDocumentDiagnostics uri diagnostics =
+  sendNotification
+    SMethod_TextDocumentPublishDiagnostics
+    (PublishDiagnosticsParams uri Nothing diagnostics)
 
 analyzeDocument :: Uri -> Text.Text -> LspM Config ()
 analyzeDocument uri contents =
-  liftIO $ do
-    hPutStrLn stderr ("analyzing:" <> show uri)
-    hPutStrLn stderr ("analysis characters:" <> show (Text.length contents))
+  case uriToFilePath uri of
+    Nothing ->
+      liftIO $
+        hPutStrLn
+          stderr
+          ("cannot analyze non-file URI: " <> show uri)
+    Just filePath -> do
+      liftIO $
+        hPutStrLn stderr ("analyzing: " <> filePath)
+      result <-
+        liftIO $
+          runFixenM $
+            pipeline
+              filePath
+              (Text.unpack contents)
+              (\_warning -> pure ())
+      case result of
+        Left compilerDiagnostics -> do
+          let lspDiagnostics =
+                map
+                  toLspDiagnostic
+                  ( Diagnostics.toFixenDiagnostics
+                      compilerDiagnostics
+                  )
+          liftIO $
+            hPutStrLn
+              stderr
+              ( "analysis failed with "
+                  <> show (length lspDiagnostics)
+                  <> " diagnostic(s)"
+              )
 
+          sendDocumentDiagnostics uri lspDiagnostics
+        Right _compilerOutput -> do
+          liftIO $
+            hPutStrLn stderr "analysis succeeded"
 
-
-
-
-
-
-
-
-
-
-
-
+          sendDocumentDiagnostics uri []
